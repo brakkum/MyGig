5917e5d5151f677d84b4a08c8ccb0d7d6f6da0d2
Daniel Brakke <brakphoto@gmail.com>
2019-03-09 14:13:34 -0600

    password check on signup

--- src/FormComponents/SignUpForm.js
+++ src/FormComponents/SignUpForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Input from "../HelperComponents/Input";
+import Button from "../HelperComponents/Button";
 
 export default class SignUpForm extends React.Component {
 
@@ -9,12 +10,34 @@ export default class SignUpForm extends React.Component {
         email: "",
         password: "",
         passwordConfirm: "",
+        passwordError: false,
+        sendingRequest: false
     };
 
     updateValue = (name, value) => {
         this.setState({
             [name]: value
         });
+    };
+
+    attemptSignUp = () => {
+        let formValid = true;
+        if (this.state.password !== this.state.passwordConfirm) {
+            this.setState({
+                passwordError: true
+            });
+            formValid = false;
+        } else {
+            this.setState({
+                passwordError: false
+            });
+        }
+        // if it's valid, send it off
+        if (formValid) {
+            this.setState({
+                sendingRequest: true
+            });
+        }
     };
 
     render() {
@@ -36,19 +59,30 @@ export default class SignUpForm extends React.Component {
                     for={"Email"}
                     value={this.state.email}
                     name={"email"}
+                    type={"email"}
                     onChange={email => this.updateValue("email", email)}
                 />
                 <Input
                     for={"Password"}
                     value={this.state.password}
                     name={"password"}
+                    type={"password"}
                     onChange={password => this.updateValue("password", password)}
                 />
                 <Input
                     for={"Confirm Password"}
                     value={this.state.passwordConfirm}
                     name={"passwordconfirm"}
+                    type={"password"}
+                    errorOverride={this.state.passwordError && "Passwords do not match"}
                     onChange={password => this.updateValue("passwordConfirm", password)}
+                />
+                <Button
+                    onClick={this.attemptSignUp}
+                    innerText={"Submit"}
+                    style={{float: "right"}}
+                    type={"success"}
+                    sendingRequest={this.state.sendingRequest}
                 />
             </div>
         )
