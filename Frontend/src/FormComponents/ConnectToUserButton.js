f97e5df59873fe5fd1444ca1521fa6c36b6ee71c
Daniel Brakke <brakphoto@gmail.com>
2019-04-17 08:26:08 -0500

    connect button state

--- Frontend/src/FormComponents/ConnectToUserButton.js
+++ Frontend/src/FormComponents/ConnectToUserButton.js
@@ -2,7 +2,13 @@ import React from "react";
 
 export default class ConnectToUserButton extends React.Component {
 
+    state = {
+        sendingRequest: false,
+        requestSent: false
+    };
+
     sendConnectionRequest = id => {
+        this.setState({sendingRequest: true});
         fetch("/api/users/newConnection", {
             method: "post",
             headers: new Headers({
@@ -16,20 +22,29 @@ export default class ConnectToUserButton extends React.Component {
             .then(json => {
                 if (json.success){
                     console.log("requested ", json);
+                    this.setState({
+                        requestSent: true
+                    })
                 } else {
                     console.log("fail, ", json);
                 }
             }).catch(e => console.log(e));
+        this.setState({sendingRequest: false});
     };
 
     render() {
         return(
             <div className="is-hoverable">
                 <button
-                    className="button is-link is-small"
+                    className={
+                        "button " +
+                        "is-small " +
+                        (this.state.sendingRequest ? "is-loading " : "") +
+                        (this.state.requestSent ? "is-success " : "is-info ")
+                    }
                     onClick={() => this.sendConnectionRequest(this.props.id)}
                 >
-                    Connect
+                    {this.state.requestSent ? "Requested" : "Connect"}
                 </button>
             </div>
         )
